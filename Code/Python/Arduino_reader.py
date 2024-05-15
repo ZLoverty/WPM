8214a78fe7bb41a02cfc2867b46a6d00c8771440
Zhengyang Liu <liuzy19911112@gmail.com>
2024-05-14 23:07:58 -0400

    write to file

--- Code/Python/Arduino_reader.py
+++ Code/Python/Arduino_reader.py
@@ -1,10 +1,22 @@
-import numpy as np
-import matplotlib.pyplot as plt
+"""
+This script reads the text from an Arduino line-by-line and print in a local file in append ("a") mode. The current time string is prefixed to each line for reference. 
 
-plt.ion()
-for i in range(100):
-    x = np.linspace(i, i+3)
-    y = np.sin(x)
-    plt.plot(x, y)
-    plt.draw()
-    plt.pause(.1)
+Features to be added: 
+- total recording time
+- custom output file
+- smart warnings 
+"""
+
+import serial
+import time
+import sys
+
+serialCom = serial.Serial("COM4", 9600)
+
+
+while True:
+    s_bytes = serialCom.readline()
+    decoded_bytes = s_bytes.decode("utf-8").strip("\r\n")
+    print(decoded_bytes)
+    with open(r"C:\Users\liuzy\Downloads\rec.txt", "a") as f:
+        f.write(time.asctime() + ": " + decoded_bytes + "\n")

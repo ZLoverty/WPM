da2b45a12ea4d7becf82e32798894dda8d722d4f
Zhengyang Liu <liuzy19911112@gmail.com>
2024-08-26 22:36:36 -0400

    finish dimple time curvature note

--- Code/Python/run.py
+++ Code/Python/run.py
@@ -1,19 +1,23 @@
 import os
 import numpy as np
 
-area_list = np.linspace(2e-4, 10e-4, 5)
-Pi0_list = np.linspace(-10, -40, 5)
-h0_list = np.linspace(2e-4, 3e-4, 20)
+area_list = [4e-4]
+Pi0_list = [-40]
+h0_list = np.linspace(1e-4, 8e-4, 40)
+viscosity_list = [10e-3, 15e-3, 1e-3]
+st_list = [42e-3, 27e-3, 72e-3]
 kappa = 1.2e-7    # Proportionality constant for contact line motion
 theta_s = np.deg2rad(50) # Equilibrium contact angle (degrees)
 
-folder = r"E:\WPM\Simulation\Scan_initial_thickness"
+folder = r"C:\Users\liuzy\Documents\WPM_Simulation\mu_sigma"
 
 for area in area_list :
     for Pi0 in Pi0_list:
         for h0 in h0_list:
-            # print the file name
-            print(f'area_{area:.1e}_Pi0_{Pi0:.1f}_h0_{h0:.2e}.csv')
-            # Run the simulation
-            filename = os.path.join(folder, f'area_{area:.1e}_Pi0_{Pi0:.1f}_h0_{h0:.2e}.csv')
-            os.system(f'python dimple_simulation.py {filename} -a {area:.1e} --kappa {kappa:.1e} --Pi0 {Pi0:.1f} --theta_s {theta_s:.1f} --h0 {h0:.2e}')
+            for mu, sigma in zip(viscosity_list, st_list):
+                # print the file name
+                filename = f'mu_{mu:.3f}_sigma_{sigma:.3f}_h0_{h0:.2e}.csv'
+                print(filename)
+                # Run the simulation
+                fileDir = os.path.join(folder, filename)
+                os.system(f'python dimple_simulation.py {fileDir} -a {area:.1e} --kappa {kappa:.1e} --Pi0 {Pi0:.1f} --theta_s {theta_s:.1f} --h0 {h0:.2e} --mu {mu:.3f} --sigma {sigma:.3f}')

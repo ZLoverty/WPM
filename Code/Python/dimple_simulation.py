97dd647e72c4cbc755b0c954f044f246d724f65b
Zhengyang Liu <liuzy19911112@gmail.com>
2024-11-05 18:28:38 -0500

    add boundary condition file reader
    - but I'm not really planning to use it

--- Code/Python/dimple_simulation.py
+++ Code/Python/dimple_simulation.py
@@ -18,24 +18,36 @@ optional arguments:
 # initial state
     --h0 : Initial film thickness, default to 2e-4
 
-
 # simulation parameters
     -T, --time : total time of the simulation (s), default to 100
     -X, --X : size of the domain (m), default to 1e-2
     -N, --number : number of spatial discretization, default to 100
     -s, --save_time : Save the states every save_time (s), default to .1
-    
+    --bc: The path to the json file that specifies the boundary conditions.
+        ``` bc.json
+        {
+            "p_left": {
+                "type": "Neumann",
+                "value": 0.0
+            },
+            "p_right": {
+                "type": "Neumann",
+                "value": 0.0
+            },
+            "h_right": {
+                "type": "Dirichlet",
+                "value": 0.0
+            }
+        }
+        ```
+
 # physical consts
     --mu : viscosity, Pa s, default to 1e-2
     --g : gravitational acceleration, m/s^2, default to 9.8
     --sigma : surface tension, N/m, default to 42e-3
     --rho : density of water, kg/m^3, default to 997
-
-# free parameters
     -k, --kappa : Proportionality constant for contact line motion, default to 0.005
-    -P, --Pi0 : Initial pressure at the left boundary (Pa), default to -10
     -t, --theta_s : Equilibrium contact angle (degrees), default to 70
-    -a, --pore_area : Area of the pores (m^2), default to 1.0e-3
 
 Example
 -------
@@ -48,7 +60,7 @@ Edit
 Aug 14, 2024: Initial commit.
 Aug 15, 2024: (i) Edit the docstring to show optional arguments more accurately. (ii) Prevent the increase of total volume due to contact line movement.
 Oct 24, 2024: Change the model from micro-pore driven to meniscus rise driven.
-Nov 05, 2024: (i) Remove unused free parameters. (ii) Convert save_file to absolute path.
+Nov 05, 2024: (i) Remove unused free parameters. (ii) Convert save_file to absolute path. (iii) Add custom boundary conditions to enable different boundary conditions using the same code.
 """
 
 import numpy as np
@@ -63,21 +75,20 @@ import pandas as pd
 parser = argparse.ArgumentParser(description='Dimple simulation')
 parser.add_argument('save_file', type=str, help='File to save the solution')
 
-# contact line parameters
-parser.add_argument('-k', '--kappa', type=float, default=0.005, help='Proportionality constant for contact line motion')
-parser.add_argument('-t', '--theta_s', type=float, default=30, help='Equilibrium contact angle (degrees)')
-
 # physical consts
 parser.add_argument('--mu', type=float, default=1e-2, help='Viscosity, Pa s')
 parser.add_argument('--g', type=float, default=9.8, help='Gravitational acceleration, m/s^2')
 parser.add_argument('--sigma', type=float, default=42e-3, help='Surface tension, N/m')
 parser.add_argument('--rho', type=float, default=997, help='Density of water, kg/m^3')
+parser.add_argument('-k', '--kappa', type=float, default=0.005, help='Proportionality constant for contact line motion')
+parser.add_argument('-t', '--theta_s', type=float, default=30, help='Equilibrium contact angle (degrees)')
 
 # simulation parameters
 parser.add_argument('-T', '--time', type=float, default=100, help='Total simulation time (s)')
 parser.add_argument('-X', '--X', type=float, default=1e-2, help='Size of the domain (m)')
 parser.add_argument('-N', '--number', type=int, default=100, help='Number of radial grid points')
 parser.add_argument('-s', '--save_time', type=float, default=.1, help='time interval between each save')
+parser.add_argument('--bc', type=str, default=None, help='The path to the json file that specifies the boundary conditions.')
 
 # initial condition
 parser.add_argument('--h0', type=float, default=2e-4, help='Initial film thickness profile')
@@ -86,15 +97,25 @@ args = parser.parse_args()
 # Read constants from command line
 save_file = os.path.abspath(args.save_file)
 
-# free parameters
-kappa = args.kappa    # Proportionality constant for contact line motion
-theta_s = np.deg2rad(args.theta_s) # Equilibrium contact angle (degrees)
+# boundary conditions
+if args.bc is not None:
+    import json
+    with open(args.bc) as f:
+        bc = json.load(f)
+    print(bc)
+    p_left = bc["p_left"]
+    p_right = bc["p_right"]
+else:
+    p_left = {"type": "Neumann", "value": 0.0}
+    p_right = {"type": "Neumann", "value": 0.0}
 
 # Physical
 sigma = args.sigma  # Surface tension (N/m)
 mu = args.mu    # Viscosity (Pa.s)
 rho = args.rho     # Density of water (kg/m^3)
 g = args.g       # Gravitational acceleration (m/s^2)
+kappa = args.kappa    # Proportionality constant for contact line motion
+theta_s = np.deg2rad(args.theta_s) # Equilibrium contact angle (degrees)
 
 # simulation
 T = args.time  # Total simulation time (s)
@@ -124,7 +145,7 @@ def film_drainage(t, y, x, X, rho, g, mu, sigma, theta_s):
     dhdt[1:-1] = 1 / (12 * mu) * ((h3[2:] - h3[:-2]) * (p[2:] - p[:-2]) + 4 * h3[1:-1] * (p[2:] - 2 * p[1:-1] + p[:-2])) / dx**2 
 
     # boundary conditions
-    U_cl = contact_line_velocity(h, x, sigma, mu, theta_s, kappa=kappa)
+    U_cl = contact_line_velocity(h, x, sigma, mu, theta_s, kappa)
     dhdt[0] = U_cl
 
     # conserve mass
@@ -141,14 +162,24 @@ def YL_equation(h, x, sigma):
     d2h = (h[2:] - 2 * h[1:-1] + h[:-2]) / dx**2
     p = np.zeros_like(h)
     p[1:-1] = - sigma * (1 + dh**2)**(-3/2) * d2h + rho * g * h[1:-1]
-    # p[1:-1] = - sigma * d2h
+
     # Boundary conditions
-    # p[0] =  p_left(h, x, V0, Pi0, pore_area)
-    p[0] = p[1]
-    p[-1] = p[-2]
+    if p_left["type"] == "Neumann":
+        p[0] = p[1] + p_left["value"] * (x[0] - x[1])
+    elif p_left["type"] == "Dirichlet":
+        p[0] = p_left["value"]
+    else:
+        raise ValueError("Unknown boundary condition")
+    if p_right["type"] == "Neumann":
+        p[-1] = p[-2] + p_right["value"] * (x[-1] - x[-2])
+    elif p_right["type"] == "Dirichlet":
+        p[-1] = p_right["value"]
+    else:
+        raise ValueError("Unknown boundary condition")
+
     return p
 
-def contact_line_velocity(h, x, sigma, mu, theta_s, kappa=1e-7):
+def contact_line_velocity(h, x, sigma, mu, theta_s, kappa):
     """
     The contact line motion model of Kim et al. (2017)
     """
@@ -170,17 +201,6 @@ def compute_contact_angle_0(h, x):
         if angle < 0:
             angle += np.pi
         return angle
-
-def p_left(h, x, V0, Pi0, pore_area):
-    """
-    Calculate the pressure at the left boundary
-    """
-    # Calculate the total volume of liquid in the domain
-    V = np.trapz(h, x)
-
-    # Calculate the pressure at the left boundary
-    p = Pi0 + rho * g * (V0 - V) / pore_area
-    return p
 
 # Solve the PDE using solve_ivp with the BDF method
 nSave = int(T / save_time)

0259896b764cdef8bba3ec8f66658b95b5943145
Zhengyang Liu <liuzy19911112@gmail.com>
2025-04-01 18:00:52 -0400

    understand the time scale

--- Code/Python/run.py
+++ Code/Python/run.py
@@ -1,16 +1,17 @@
 import os
 import numpy as np
+import time
 
 # h0_list = np.logspace(np.log10(0.2e-3), np.log10(1e-3), 100)
-h0_list = [0.3e-3]
-st_list = [42e-3]
+h0_list = np.linspace(0.2e-3, 0.6e-3, 15)
+st_list = np.linspace(10e-3, 100e-3, 15)  # sigma from 0.01 to 0.1 in 5 steps
 viscosity_list = [1e-2]
-L_list = np.logspace(np.log10(0.01), np.log10(.1), 30)  # L from 0.2 to 1.0 in 5 steps
+L_list = np.linspace(1e-2, 1e-1, 15)  # L from 0.2 to 1.0 in 5 steps
 theta_s_list = [17.4]
 kappa_list = [2.2e-4]
 
 
-folder = r"G:\My Drive\Research projects\WPM\Data\Simulation\parameters\length"
+folder = r"C:\Users\zl948\Documents\WPM"
 
 for kappa, theta_s in zip(kappa_list, theta_s_list):
     for mu in viscosity_list:
@@ -20,7 +21,7 @@ for kappa, theta_s in zip(kappa_list, theta_s_list):
                     # print the file name
                     N = max(500, int(1000 * L / 0.2))
                     filename = f'mu_{mu:.2e}_sigma_{sigma:.2e}_h0_{h0:.2e}_L_{L:.2e}.csv'
-                    print(filename)
+                    print(time.asctime() + " -> " + filename)
                     # Run the simulation
                     fileDir = os.path.join(folder, filename)
                     os.system(f'python dimple_simulation.py \"{fileDir}\" --kappa {kappa:.2e}  --theta_s {theta_s:.2e} --h0 {h0:.2e} --mu {mu:.2e} --sigma {sigma:.2e} -X {L:.2e} -T 200 -s 0.1 -N {N:d}')

7cb2fcef3a1ec58a6f3d6ac147786335c803b5d6
Zhengyang Liu <liuzy19911112@gmail.com>
2025-04-03 19:02:35 -0400

    still working on the time scale
    - got the idea to make phase diagrams, so we have better understanding of which force scale to use.

--- Code/Python/run.py
+++ Code/Python/run.py
@@ -3,15 +3,15 @@ import numpy as np
 import time
 
 # h0_list = np.logspace(np.log10(0.2e-3), np.log10(1e-3), 100)
-h0_list = np.linspace(0.2e-3, 0.6e-3, 15)
-st_list = np.linspace(10e-3, 100e-3, 15)  # sigma from 0.01 to 0.1 in 5 steps
+h0_list = np.linspace(0.2e-3, 0.4e-3, 30)
+st_list = [42e-3]  # sigma from 0.01 to 0.1 in 5 steps
 viscosity_list = [1e-2]
-L_list = np.linspace(1e-2, 1e-1, 15)  # L from 0.2 to 1.0 in 5 steps
+L_list = np.linspace(15e-3, 50e-3, 30)  # L from 0.2 to 1.0 in 5 steps
 theta_s_list = [17.4]
 kappa_list = [2.2e-4]
 
 
-folder = r"C:\Users\zl948\Documents\WPM"
+folder = r"C:\Users\zl948\Documents\WPM_PD2"
 
 for kappa, theta_s in zip(kappa_list, theta_s_list):
     for mu in viscosity_list:
@@ -19,7 +19,7 @@ for kappa, theta_s in zip(kappa_list, theta_s_list):
             for h0 in h0_list:
                 for L in L_list:
                     # print the file name
-                    N = max(500, int(1000 * L / 0.2))
+                    N = 100
                     filename = f'mu_{mu:.2e}_sigma_{sigma:.2e}_h0_{h0:.2e}_L_{L:.2e}.csv'
                     print(time.asctime() + " -> " + filename)
                     # Run the simulation

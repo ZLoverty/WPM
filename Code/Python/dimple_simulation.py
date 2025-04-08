94c82a96dc83760c4b0f90a42f50daae79407cb9
Zhengyang Liu <liuzy19911112@gmail.com>
2025-04-08 17:56:38 -0400

    Simplify the code by
    - fix BC as Neumann;
    - Use operators and sparse matrices for computing derivatives;
    - Remove redundant arguments.

--- Code/Python/dimple_simulation.py
+++ Code/Python/dimple_simulation.py
@@ -23,23 +23,6 @@ optional arguments:
     -X, --X : size of the domain (m), default to 1e-2
     -N, --number : number of spatial discretization, default to 100
     -s, --save_time : Save the states every save_time (s), default to .1
-    --bc: The path to the json file that specifies the boundary conditions.
-        ``` bc.json
-        {
-            "p_left": {
-                "type": "Neumann",
-                "value": 0.0
-            },
-            "p_right": {
-                "type": "Neumann",
-                "value": 0.0
-            },
-            "h_right": {
-                "type": "Dirichlet",
-                "value": 0.0
-            }
-        }
-        ```
 
 # physical consts
     --mu : viscosity, Pa s, default to 1e-2
@@ -49,12 +32,6 @@ optional arguments:
     -k, --kappa : Proportionality constant for contact line motion, default to 0.005
     -t, --theta_s : Equilibrium contact angle (degrees), default to 70
 
-Example
--------
-
-python dimple_simulation.py E:\WPM\Simulation\Scan_free_params\kappa_1.0e-06_Pi0_0.0_theta_s_0.0.csv --kappa 1.0e-06 --Pi0 0.0 --theta_s 0.0
-
-
 Edit
 ----
 Aug 14, 2024: Initial commit.
@@ -62,6 +39,7 @@ Aug 15, 2024: (i) Edit the docstring to show optional arguments more accurately.
 Oct 24, 2024: Change the model from micro-pore driven to meniscus rise driven.
 Nov 05, 2024: (i) Remove unused free parameters. (ii) Convert save_file to absolute path. (iii) Add custom boundary conditions to enable different boundary conditions using the same code.
 Apr 03, 2025: Change default liquid properties to beet juice properties.
+Apr 08, 2025: (i) Simplify the code: fix BC as Neumann; (ii) Use operators and sparse matrices for computing derivatives; (iii) Remove redundant arguments.
 """
 
 import numpy as np
@@ -71,6 +49,7 @@ import os
 import argparse
 import pdb
 import pandas as pd
+from scipy.sparse import diags, kron, identity
 
 # Read constants from command line
 parser = argparse.ArgumentParser(description='Dimple simulation')
@@ -85,11 +64,11 @@ parser.add_argument('-k', '--kappa', type=float, default=2.2e-4, help='Proportio
 parser.add_argument('-t', '--theta_s', type=float, default=17.4, help='Equilibrium contact angle (degrees)')
 
 # simulation parameters
-parser.add_argument('-T', '--time', type=float, default=100, help='Total simulation time (s)')
-parser.add_argument('-X', '--X', type=float, default=1e-2, help='Size of the domain (m)')
-parser.add_argument('-N', '--number', type=int, default=100, help='Number of radial grid points')
+parser.add_argument('-T', '--time', type=float, default=200, help='Total simulation time (s)')
+parser.add_argument('-X', '--X', type=float, default=24e-3, help='Size of the domain (m)')
+parser.add_argument('-N', '--number', type=int, default=200, help='Number of radial grid points')
 parser.add_argument('-s', '--save_time', type=float, default=.1, help='time interval between each save')
-parser.add_argument('--bc', type=str, default=None, help='The path to the json file that specifies the boundary conditions.')
+# parser.add_argument('--bc', type=str, default=None, help='The path to the json file that specifies the boundary conditions.')
 
 # initial condition
 parser.add_argument('--h0', type=float, default=2e-4, help='Initial film thickness profile')
@@ -98,17 +77,17 @@ args = parser.parse_args()
 # Read constants from command line
 save_file = os.path.abspath(args.save_file)
 
-# boundary conditions
-if args.bc is not None:
-    import json
-    with open(args.bc) as f:
-        bc = json.load(f)
-    print(bc)
-    p_left = bc["p_left"]
-    p_right = bc["p_right"]
-else:
-    p_left = {"type": "Neumann", "value": 0.0}
-    p_right = {"type": "Neumann", "value": 0.0}
+# # boundary conditions
+# if args.bc is not None:
+#     import json
+#     with open(args.bc) as f:
+#         bc = json.load(f)
+#     print(bc)
+#     p_left = bc["p_left"]
+#     p_right = bc["p_right"]
+# else:
+#     p_left = {"type": "Neumann", "value": 0.0}
+#     p_right = {"type": "Neumann", "value": 0.0}
 
 # Physical
 sigma = args.sigma  # Surface tension (N/m)
@@ -132,22 +111,28 @@ h[:-1] = args.h0 # Initial film thickness profile
 # pre-simulation calculation
 V0 = np.trapz(h, x)
 
-def film_drainage(t, y, x, X, rho, g, mu, sigma, theta_s):
+dx = x[1] - x[0]  # Grid spacing
+# 1D first derivative operator (2nd order accuracy)
+Dx = diags([-1, 1], [-1, 1], shape=(N, N), format='csr')
+Dx = Dx / (2*dx)
+# 1D second derivative operator (2nd order accuracy)
+D2x = diags([1, -2, 1], [-1, 0, 1], shape=(N, N), format='csr')
+D2x = D2x / dx**2
+
+def film_drainage(t, y):
 
     h = y
     
-    dx = (x[2:] - x[:-2]) / 2
-    
-    p = YL_equation(h, x, sigma)
+    p = YL_equation(h)
     
     # Reynolds equation
-    dhdt = np.zeros(h.shape)
-    h3 =  h**3
-    dhdt[1:-1] = 1 / (12 * mu) * ((h3[2:] - h3[:-2]) * (p[2:] - p[:-2]) + 4 * h3[1:-1] * (p[2:] - 2 * p[1:-1] + p[:-2])) / dx**2 
+    # h3 =  h**3
+    # dhdt[1:-1] = 1 / (12 * mu) * ((h3[2:] - h3[:-2]) * (p[2:] - p[:-2]) + 4 * h3[1:-1] * (p[2:] - 2 * p[1:-1] + p[:-2])) / dx**2 
+    dhdt = 1 / (3*mu) * ( 3 * h**2 * (Dx @ h) * (Dx @ p) + h**3 * (D2x @ p) )  # 1D Reynolds equation)
 
     # boundary conditions
-    U_cl = contact_line_velocity(h, x, sigma, mu, theta_s, kappa)
-    dhdt[0] = U_cl
+    dhdt[0] = contact_line_velocity(h)
+    dhdt[-1] = 0
 
     # conserve mass
     if dhdt.sum() != 0:
@@ -155,46 +140,29 @@ def film_drainage(t, y, x, X, rho, g, mu, sigma, theta_s):
     
     return dhdt
 
-def YL_equation(h, x, sigma):
+def YL_equation(h):
 
-    # Young-Laplace equation
-    dx = (x[2:] - x[:-2]) / 2
-    dh = (h[2:] - h[:-2]) / dx / 2
-    d2h = (h[2:] - 2 * h[1:-1] + h[:-2]) / dx**2
-    p = np.zeros_like(h)
-    p[1:-1] = - sigma * (1 + dh**2)**(-3/2) * d2h + rho * g * h[1:-1]
-    # p[1:-1] = - sigma * d2h + rho * g * h[1:-1]
+    p = - sigma * (1 + (Dx @ h)**2)**(-3/2) * (D2x @ h) + rho * g * h
+    # p = - sigma * (D2x @ h) + rho * g * h / 2
 
-    # Boundary conditions
-    if p_left["type"] == "Neumann":
-        p[0] = p[1] + p_left["value"] * (x[0] - x[1])
-    elif p_left["type"] == "Dirichlet":
-        p[0] = p_left["value"]
-    else:
-        raise ValueError("Unknown boundary condition")
-    if p_right["type"] == "Neumann":
-        p[-1] = p[-2] + p_right["value"] * (x[-1] - x[-2])
-    elif p_right["type"] == "Dirichlet":
-        p[-1] = p_right["value"]
-    else:
-        raise ValueError("Unknown boundary condition")
+    p[0] = p[1]
+    p[-1] = p[-2]
 
     return p
 
-def contact_line_velocity(h, x, sigma, mu, theta_s, kappa):
+def contact_line_velocity(h):
     """
     The contact line motion model of Kim et al. (2017)
     """
-    theta = compute_contact_angle_0(h, x)
+    theta = compute_contact_angle_0(h)
     U_cl = kappa / mu * sigma * theta * (theta**2 - theta_s**2)
     return U_cl
 
-def compute_contact_angle_0(h, x):
+def compute_contact_angle_0(h):
     """
     Calculate the contact angle based on the film thickness profile
     """
     dh = h[1] - h[0]
-    dx = x[1] - x[0]
 
     if dh == 0:
         return np.pi / 2
@@ -207,7 +175,7 @@ def compute_contact_angle_0(h, x):
 # Solve the PDE using solve_ivp with the BDF method
 nSave = int(T / save_time)
 t_eval = np.linspace(0, T, nSave)
-solution = solve_ivp(film_drainage, [0, T], h, method='BDF', t_eval=t_eval, args=(x, X, rho, g, mu, sigma, theta_s), atol=1e-6, rtol=1e-6)  # BDF method is suitable for stiff problems
+solution = solve_ivp(film_drainage, [0, T], h, method='BDF', t_eval=t_eval, atol=1e-6, rtol=1e-6)  # BDF method is suitable for stiff problems
 
 # create save_folder
 save_folder = os.path.dirname(save_file)
